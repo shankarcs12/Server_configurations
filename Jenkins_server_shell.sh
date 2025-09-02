6f94c59b6c2e686dc8dc703398c67e0a3fc99bd4
shankarcs12 <shankarcs12@gmail.com>
2025-09-03 00:47:07 +0530

    Jenkins_server_shell.sh commit by repository

---
+++ Jenkins_server_shell.sh
@@ -0,0 +1,9 @@
+yum install java-17-amazon-correctto -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+systemctl start jenkins
+systemctl enable jenkins
+systemctl status jenkins
+yum install git -y
