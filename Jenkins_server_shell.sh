3fd313475dd9a5255f491fdc6d9e26190e178f20
shankarcs12 <shankarcs12@gmail.com>
2025-09-03 01:06:03 +0530

    Update Jenkins_server_shell.sh

--- Jenkins_server_shell.sh
+++ Jenkins_server_shell.sh
@@ -1,9 +1,17 @@
+# Installing the Java-17 version
 yum install java-17-amazon-corretto -y
+#Installing  git
+yum install git -y
+# jenkins repository download
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# Installing jenkins
 yum install jenkins -y
+# To Start the jenkins
 systemctl start jenkins
+# To enable the jenkins
 systemctl enable jenkins
+# Check the status of the jenkins service
 systemctl status jenkins
-yum install git -y
+
