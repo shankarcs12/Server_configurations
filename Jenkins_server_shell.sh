de8c2566e56a3472d6293b67ad32c3fc5b870b8f
shankarcs12 <shankarcs12@gmail.com>
2026-07-14 03:59:10 +0530

    Update Jenkins_server_shell.sh

--- Jenkins_server_shell.sh
+++ Jenkins_server_shell.sh
@@ -1,11 +1,14 @@
 # Installing the Java-17 version
-yum install java-17-amazon-corretto -y
+yum install java-21-amazon-corretto -y
 #Installing  git
 yum install git -y
 # jenkins repository download
+# sudo wget -O /etc/yum.repos.d/jenkins.repo \
+ #   https://pkg.jenkins.io/redhat-stable/jenkins.repo
+#sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+    https://pkg.jenkins.io/rpm-stable/jenkins.repo
+sudo yum upgrade
 # Installing jenkins
 yum install jenkins -y
 # To Start the jenkins
