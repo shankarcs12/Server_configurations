6fa52f86c5b4174095d44967239b4b6e13b9b175
shankarcs12 <shankarcs12@gmail.com>
2026-07-14 04:11:27 +0530

    Update tomcat_server_shell.sh

--- tomcat_server_shell.sh
+++ tomcat_server_shell.sh
@@ -1,7 +1,8 @@
 # install java
-yum install java-17-amazon-corretto -y
+yum install java-21-amazon-corretto -y
 # get the apache resources from the website
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.108/bin/apache-tomcat-9.0.108.tar.gz
+# wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.108/bin/apache-tomcat-9.0.108.tar.gz
+wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.118/bin/apache-tomcat-9.0.118.tar.gz
 #unzip the file
-tar -zxvf apache-tomcat-9.0.108.tar.gz
-cd apache-tomcat-9.0.108
+tar -zxvf apache-tomcat-9.0.118.tar.gz
+cd apache-tomcat-9.0.118
