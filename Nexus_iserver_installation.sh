85b38df46b24554c677d9ab043e189a3b70dd2cd
shankarcs12 <shankarcs12@gmail.com>
2026-07-15 11:57:14 +0530

    Update Nexus_iserver_installation.sh

--- Nexus_iserver_installation.sh
+++ Nexus_iserver_installation.sh
@@ -3,17 +3,17 @@ yum install java-21-amazon-corretto -y
 # navigate to /opt folder
 cd /opt
 # download the nexus files from the site by using wget cmd
-wget https://download.sonatype.com/nexus/3/nexus-3.85.0-03-linux-x86_64.tar.gz
+wget https://download.sonatype.com/nexus/3/nexus-3.94.0-12-linux-x86_64.tar.gz
 # use ls to list out the files and folders
 ls -l
 # unzip file
-tar -zxvf nexus-3.85.0-03-linux-x86_64.tar.gz
+tar -zxvf nexus-3.94.0-12-linux-x86_64.tar.gz
 # create a user account with nexus by using useradd cmd
 useradd nexus
 # change the owner of the directories nexus-3.85.0-03,sonatype-work
-chown -R nexus:nexus nexus-3.85.0-03 sonatype-work
-# navigate to nexus-3.85.0-03/bin
-cd nexus-3.85.0-03/bin
+chown -R nexus:nexus nexus-3.94.0-12 sonatype-work
+# navigate to nexus-3.94.0-12/bin
+cd nexus-3.94.0-12/bin
 #open nexus file
 vi nexus
 
